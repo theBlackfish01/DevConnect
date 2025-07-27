2285aac061465f08a86dd1d7adbc65520606732d
wadood <wadood2003@gmail.com>
2025-07-27 19:09:18 +0500

    routes, models frameworks

---
+++ server.js
@@ -0,0 +1,14 @@
+// @ts-ignore
+const express   = require('express');
+const connectDB = require('./config/db.js');
+
+const app  = express();
+const port = process.env.PORT || 3000;
+connectDB();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/post'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
