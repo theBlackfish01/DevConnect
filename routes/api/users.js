c53947a43279041a0088dad2c46ab99e93cc919e
wadood <wadood2003@gmail.com>
2025-07-28 14:35:42 +0500

    routes, models frameworks

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 
@@ -25,19 +28,49 @@ router.post('/', [
 
     try{
         // check user exists
+        let user = await User.findOne({ email});
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        }
 
         // get users gravatar
+        const avatar = gravatar.url(email, {
+            s: '200', // size
+            r: 'pg', // rating
+            d: 'mm' // default
+        });
+
+        // create user
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
 
         // encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        console.log('User registered')
 
         // return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
 
+        const token = jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 36000 // 10 hours
+        });
+        return res.json({ token });
     } catch(err) {
         console.error(err.message);
-        res.status(500).send('Server Error');
+        return res.status(500).send('Server Error');
     }
-
-    res.send('User Route')
 });
 
 module.exports = router;
