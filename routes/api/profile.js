5317af16828b3602c09c143dbbdbbdd9d18055f6
wadood <wadood2003@gmail.com>
2025-07-31 00:49:28 +0500

    profile routes experience options

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,59 @@ router.delete('/', auth, async (req, res) => {
     }
 })
 
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { title, company, location, from, to, current, description } = req.body;
+
+    // Create experience object
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExp);
+        await profile.save();
+        return res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error (Profile PUT Experience)');
+    }
+})
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        if (removeIndex === -1) {
+            return res.status(400).json({ msg: 'Experience not found' });
+        }
+        profile.experience.splice(removeIndex, 1);
+        await profile.save();
+        return res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error (Profile DELETE Experience)');
+    }
+})
+
 module.exports = router;
